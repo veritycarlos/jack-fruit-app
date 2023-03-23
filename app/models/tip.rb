1fddbddaf446c93881580a1bf5f31f80985c58fd
Verity Collins <verity.carlos@gmail.com>
2023-03-23 15:16:30 -0500

    updated plant model and migrated

--- app/models/tip.rb
+++
@@ -1,6 +0,0 @@
-class Tip < ApplicationRecord
-  validates :comment, :title, :rating, presence: true
-
-  belongs_to :user
-  belongs_to :plant
-end
--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-    validates :username, :password, presence: true
-
-    has_secure_password
-
-    has_many :tips
-    has_many :plants, through: :tips
-end
